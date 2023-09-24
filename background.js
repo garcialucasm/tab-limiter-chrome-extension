a2f8a1d1c554e915a096460fd17b45d3cbaddec2
garcialucasm <116840436+garcialucasm@users.noreply.github.com>
2023-09-24 17:24:36 +0100

    Change openTabs storage type to local storage

--- background.js
+++ background.js
@@ -1,19 +1,45 @@
-// Initialize an array to keep track of open tabs
-let openTabs = [];
+// Initialize an object to keep track of open tabs
+let openTabs = {};
 
-// Listen for the creation of a new tab
-chrome.tabs.onCreated.addListener((tab) => {
-  // Check if the number of open tabs has reached the limit (5)
-  if (openTabs.length >= 5) {
-    // If the limit is reached, close the newly created tab
-    chrome.tabs.remove(tab.id);
-  } else {
-    // If the limit is not reached, add the new tab's ID to the openTabs array
-    openTabs.push(tab.id);
-  }
-});
-// Listen for the removal of a tab
-chrome.tabs.onRemoved.addListener((tabId) => {
-  // Remove the closed tab's ID from the openTabs array
-  openTabs = openTabs.filter((id) => id !== tabId);
+// Function to add an open tab to openTabs
+function addOpenTab(tabId) {
+  openTabs[tabId] = true;
+  chrome.storage.local.set({ openTabs });
+}
+
+// Function to remove a closed tab from openTabs
+function removeClosedTab(tabId) {
+  delete openTabs[tabId];
+  chrome.storage.local.set({ openTabs });
+}
+
+// Function to update the openTabs array with the currently open tabs
+function updateOpenTabs() {
+  chrome.tabs.query({}, (tabs) => {
+    tabs.forEach((tab) => {
+      addOpenTab(tab.id);
+    });
+  });
+  console.log(openTabs);
+}
+
+// Load the open tabs from local storage on extension startup
+chrome.storage.local.get("openTabs", (result) => {
+  // Listen for the creation of a new tab
+  chrome.tabs.onCreated.addListener((tab) => {
+    updateOpenTabs();
+    if (Object.keys(openTabs).length >= 5) {
+      // If the limit is reached, close the newly created tab
+      chrome.tabs.remove(tab.id);
+    } else {
+      // If the limit is not reached, add the new tab's ID to the openTabs object
+      addOpenTab(tab.id);
+    }
+  });
+
+  // Listen for the removal of a tab
+  chrome.tabs.onRemoved.addListener((tabId) => {
+    updateOpenTabs();
+    removeClosedTab(tabId);
+  });
 });
